5ef8157402302bf7270a78b7e6a2212d19198256
Sandrine <svm@cesar.org.br>
2022-08-20 14:11:06 -0300

    adicionando eslint

--- math.js
+++ math.js
@@ -1,4 +1,6 @@
-const math = {};
-math.add = (num1, num2) => num1 + num2;
-math.multiply = (num1, num2) => num1 * num2;
-module.exports = math;
+const MATH = {};
+
+MATH.add = (num1, num2) => num1 + num2;
+MATH.multiply = (num1, num2) => num1 * num2;
+
+module.exports = MATH;
--- test/math.test.js
+++ test/math.test.js
@@ -1,26 +1,26 @@
 const expect = require('chai').expect;
 // import math file
-const math = require('../math');
+const MATH = require('../math');
 describe('math.js tests', () => {
     describe('math.add() Test', () => {
         it('should equal 2', () => {
-            const result = math.add(1, 1);
-            expect(result).to.equal(2);
+            const RESULT = MATH.add(1, 1);
+            expect(RESULT).to.equal(2);
         });
         it('should equal 4', () => {
-            const result = math.add(2, 2);
-            expect(result).to.equal(4);
+            const RESULT = MATH.add(2, 2);
+            expect(RESULT).to.equal(4);
         });
     });
     
     describe('math.multiply() Test', () => {
         it('should equal 3', () => {
-            const result = math.multiply(3, 1);
-            expect(result).to.equal(3);
+            const RESULT = MATH.multiply(3, 1);
+            expect(RESULT).to.equal(3);
         });
         it('should equal 10', () => {
-            const result = math.multiply(5, 2);
-            expect(result).to.equal(10);
+            const RESULT = MATH.multiply(5, 2);
+            expect(RESULT).to.equal(10);
         });
     });
 });
